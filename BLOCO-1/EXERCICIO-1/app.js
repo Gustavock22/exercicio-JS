8f8a3916ffa5fd91f953478c28e50c7a6a160e87
GUSTAVO CAMARGO MARTINS <gustavo_martins155@estudante.sesisenai.org.br>
2026-05-26 21:11:21 -0300

    DESAFIO DO BLOCO 1

---
+++ BLOCO-1/EXERCICIO-1/app.js
@@ -0,0 +1,9 @@
+const nome = "Gustavock";
+const classe = "Assassino";
+let nivel = 67;
+let hp = 100;
+let arma = "Espadas Curtas";
+let stats = "Vivo";
+
+
+console.log(`NOME: ${nome} | CLASSE: ${classe} | NÍVEL: ${nivel} | HP ${hp} | ARMA: ${arma} | STATUS: ${stats}`)
