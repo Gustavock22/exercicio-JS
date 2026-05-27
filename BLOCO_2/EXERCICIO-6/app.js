d93f758be851ee7c9ba56dfa4fb6831769688b44
Gustavock22 <gustavocm2210@gmail.com>
2026-05-26 22:17:14 -0300

    EXERCICIO 6

---
+++ BLOCO_2/EXERCICIO-6/app.js
@@ -0,0 +1,17 @@
+let i = 1;
+do{
+    console.log(i);
+    i++;
+}while(i <= 10)
+
+console.log("--------------------");
+
+do{
+    if(i % 2 != 0)
+    {
+        console.log(i);
+    }
+
+    i++;
+
+}while(i <= 20);
