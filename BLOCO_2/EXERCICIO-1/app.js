5bafb79b1bdc356f865c5b650094476b759cb0cf
Gustavock22 <gustavocm2210@gmail.com>
2026-05-26 21:43:29 -0300

    EXERCICIO 1

---
+++ BLOCO_2/EXERCICIO-1/app.js
@@ -0,0 +1,30 @@
+let num1 = 2;
+let num2 = 5;
+let num3 = 100;
+
+if(num1 % 2 == 0)
+{
+    console.log("Par");
+}
+else
+{
+    console.log("Ímpar");
+}
+
+if(num2 % 2 == 0)
+{
+    console.log("Par");
+}
+else
+{
+    console.log("Ímpar");
+}
+
+if(num3 % 2 == 0)
+{
+    console.log("Par");
+}
+else
+{
+    console.log("Ímpar");
+}
