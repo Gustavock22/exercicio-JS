135cd7e179773bd268d7888c1c58a7a6424ec49d
Gustavock22 <gustavocm2210@gmail.com>
2026-05-26 22:06:35 -0300

    EXERCICIO 5

---
+++ BLOCO_2/EXERCICIO-5/app.js
@@ -0,0 +1,6 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "Bootstrap", "Node.js"];
+
+for(let i = 0; i < tecnologias.length; i++)
+{
+    console.log(`POSIÇÃO[${i + 1}] : ITEM[${tecnologias[i]}]`);
+}
