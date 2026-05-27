814d7233ca42722f5749a84ea5272aa016c7089e
Gustavock22 <gustavocm2210@gmail.com>
2026-05-26 21:48:15 -0300

    EXERCICIO 2

---
+++ BLOCO_2/EXERCICIO-2/app.js
@@ -0,0 +1,18 @@
+let nota = 6.7;
+
+if(nota >= 9)
+{
+    console.log("Exelente")
+}
+else if(nota >= 7)
+{
+    console.log("Aprovado")
+}
+else if(nota >= 5)
+{
+    console.log("Recuperação")
+}
+else
+{
+    console.log("Reprovado")    
+}
