36bf82ff6a5e4602a5aa4fe869f364adf5099c13
Gustavock22 <gustavocm2210@gmail.com>
2026-05-28 19:36:03 -0300

    EXERCICIO 8

---
+++ BLOCO_2/EXERCICIO-8/app.js
@@ -0,0 +1,19 @@
+const frutas = ["Maçã", "Banana", "Laranja"];
+
+console.log(frutas);
+
+frutas.push("Uva");
+
+console.log(frutas);
+
+frutas.unshift("Morango");
+
+console.log(frutas);
+
+frutas.pop("Uva");
+
+console.log(frutas);
+
+frutas.shift("Morango");
+
+console.log(frutas);
