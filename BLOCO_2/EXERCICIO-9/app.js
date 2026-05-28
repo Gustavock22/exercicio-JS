14fd289c6a84227797ed736749d67647da5bfa23
Gustavock22 <gustavocm2210@gmail.com>
2026-05-28 19:52:01 -0300

    EXERCICIO 9

---
+++ BLOCO_2/EXERCICIO-9/app.js
@@ -0,0 +1,19 @@
+const temperaturas = [23, 17, 31, 8, 25, 14, 29, 11];
+
+let maior = temperaturas[0];
+let menor = temperaturas[0];
+
+for(let i = 0; i < temperaturas.length; i++){
+    if(temperaturas[i] > maior)
+    {
+        maior = temperaturas[i];
+    }
+
+    if(temperaturas[i] < menor)
+    {
+        menor = temperaturas[i];
+    }
+}
+
+console.log(`Maior: ${maior}`);
+console.log(`Menor: ${menor}`);
